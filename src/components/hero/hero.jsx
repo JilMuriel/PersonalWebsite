df1b48b28d173f6bf00cf5ec8e7eb86fc71ce8a1
Jil Muriel <djibrilmuriel@gmail.com>
2021-06-10 17:47:52 +0800

    update

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -9,6 +9,7 @@ import { ReactComponent as CircleObjects } from "./img/CircleObjects.svg";
 import CustomButton from "../custom-button/CustomButton";
 import { useSpring, animated } from "react-spring";
 import Img01 from "../img/strategy.svg";
+import Img02 from "../img/information-architecture.svg";
 import Navbar from "../navbar/navbar";
 import "./hero.css";
 
@@ -61,18 +62,54 @@ export const Hero = () => {
           </h1>
         </div>
       </div>
-      <div className="h-screen flex">
+      <div className="h-screen flex lg:px-20">
         <div className="border mx-auto lg:px-10 xl:px-32 h-auto lg:h-full flex flex-col lg:flex-row items-center">
           <div className="lg:w-1/2 flex justify-center flex-col text-center">
-            <img className="px-10" src={Img01} alt={Img01} />
+            <img className="px-20" src={Img01} alt={Img01} />
           </div>
           <div className="lg:w-1/2 flex justify-center flex-col border">
-            <div className="">
+            <div className="py-20 lg:px-32">
               <p className="text-md font-bold mb-5">Strategy</p>
               <p className="text-msm">
                 There’s no style over substance here. I bring together form and
                 the function to create something that looks good and performs
                 brilliantly. Simple.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="h-screen flex lg:px-20">
+        <div className="border mx-auto lg:px-10 xl:px-32 h-auto lg:h-full flex flex-col lg:flex-row items-center">
+          <div className="lg:w-1/2 flex justify-center flex-col border">
+            <div className="py-20 lg:px-32">
+              <p className="text-md font-bold mb-5">Information Architecture</p>
+              <p className="text-msm">
+                It’s about organizing the content and flow of a website based on
+                research and planning. Eventually I come up with a
+                structure/design that balances the users’ desires with the
+                business’s needs.
+              </p>
+            </div>
+          </div>
+          <div className="lg:w-1/2 flex justify-center flex-col text-center">
+            <img className="px-20" src={Img02} alt={Img02} />
+          </div>
+        </div>
+      </div>
+      <div className="h-screen flex lg:px-20">
+        <div className="border mx-auto lg:px-10 xl:px-32 h-auto lg:h-full flex flex-col lg:flex-row items-center">
+          <div className="lg:w-1/2 flex justify-center flex-col text-center">
+            <img className="px-20" src={Img01} alt={Img01} />
+          </div>
+          <div className="lg:w-1/2 flex justify-center flex-col border">
+            <div className="py-20 lg:px-32">
+              <p className="text-md font-bold mb-5">Design & Development</p>
+              <p className="text-msm">
+                As a designer and developer, I understand the perfect user
+                interface should look good and work even better. Alongside my
+                clients, I uncover problems and solve them. In short, I create
+                bolder online experiences.
               </p>
             </div>
           </div>
